abce53b29222e28afbf4c04f3804659e14981bd5
BarraDevs <157757822+BarraDevs@users.noreply.github.com>
2024-04-27 00:46:53 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,64 @@
+// alert("Hello World")
+/*
+let quest1Number1 = prompt("Digite um número")
+let quest1Number2 = prompt("Digite outro número")
+quest1Number1 =  Number(quest1Number1)
+quest1Number2 = Number(quest1Number2)
+let quest1sum = quest1Number1 + quest1Number2
+alert(quest1sum)
+
+*/
+/*
+let quest2valour = "a"
+let check = typeof quest2valour
+let checkNumber = check == 'number' ? "É um número" : "Não é um número"
+alert(checkNumber)
+*/
+/*
+let quest2valour = 1
+let check = typeof quest2valour
+let checkNumber = check == 'string' ? "É uma string" : "Não é uma string"
+alert(checkNumber)
+*/
+/*
+let quest2valour = true
+let check = typeof quest2valour
+let checkNumber = check == 'boolean' ? "É um boleano" : "Não é boleano"
+alert(checkNumber)
+*/
+/*
+let quest1Number1 = prompt("Digite um número")
+let quest1Number2 = prompt("Digite outro número")
+quest1Number1 =  Number(quest1Number1)
+quest1Number2 = Number(quest1Number2)
+let quest1sum = quest1Number1 - quest1Number2
+alert(quest1sum)
+*/
+/*
+let quest1Number1 = prompt("Digite um número")
+let quest1Number2 = prompt("Digite outro número")
+quest1Number1 =  Number(quest1Number1)
+quest1Number2 = Number(quest1Number2)
+let quest1sum = quest1Number1 * quest1Number2
+alert(quest1sum)
+*/
+/*
+let quest1Number1 = prompt("Digite um número")
+let quest1Number2 = prompt("Digite outro número")
+quest1Number1 =  Number(quest1Number1)
+quest1Number2 = Number(quest1Number2)
+let quest1sum = quest1Number1 / quest1Number2
+alert(quest1sum)
+*/
+/*
+let quest2valour = 4
+let check = quest2valour % 2
+let checkNumber = check == 0 ? "É um número é par" : "Não é um número par"
+alert(checkNumber)
+*/
+/*
+let quest2valour = 4
+let check = quest2valour % 2
+let checkNumber = check != 0 ? "É um número impar" : "Não é um número impar"
+alert(checkNumber)
+*/
